3ad807b34082596723651afebcc17eef86f5c221
Ciara Duncum <cduncumnz@gmail.com>
2022-06-27 16:59:53 +1200

    header image displaying

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,13 +1,19 @@
 import React from "react"
 
 
+
+
 function Header () {
   return(
 <>
+<div className="header">
 <h1>The Silhouette</h1>
-<img src='server/public/images/silhouette-logo.png' alt='the silhouette logo'/>
+<h2>Only the finest fake news</h2>
+<img className="logo" src='images/silhouette-logo.png' alt='the silhouette logo'/>
+</div>
 </>
 )
 }
+
 
 export default Header
